9c68112c2fdbabc7bdd388c107b2bce49aeccf60
MrRetyNine <we.are.beautiful@yandex.ru>
2022-11-30 03:05:53 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,7 +11,22 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for c in plaintext:
+        if c.isupper():  # проверить, является ли символ прописным
+            c_index = ord(c) - ord("A")
+            # сдвиг текущего символа на позицию key
+            c_shifted = (c_index + shift) % 26 + ord("A")
+            c_new = chr(c_shifted)
+            ciphertext += c_new
+        elif c.islower():  # проверка наличия символа в нижнем регистре
+            # вычесть юникод 'a', чтобы получить индекс в диапазоне [0-25)
+            c_index = ord(c) - ord("a")
+            c_shifted = (c_index + shift) % 26 + ord("a")
+            c_new = chr(c_shifted)
+            ciphertext += c_new
+        else:
+            # если нет алфавита, оставьте все как есть
+            ciphertext += c
     return ciphertext
 
 
@@ -27,6 +42,5 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+    plaintext = encrypt_caesar(ciphertext, -shift)
     return plaintext
