a5fdbe1dcc565c2e383994628a43d0fd47cd54c7
MrRetyNine <we.are.beautiful@yandex.ru>
2023-03-20 21:12:29 +0300

    fix5

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -4,13 +4,8 @@ from ui import UI
 
 
 class GUI(UI):
-    def __init__(
-        self,
-        life: GameOfLife,
-        cell_size: int = 20,
-        speed: int = 10,
-    ) -> None:
-
+    def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
+        
         self.width = life.cols * cell_size
         self.height = life.rows * cell_size
         self.cell_size = cell_size
