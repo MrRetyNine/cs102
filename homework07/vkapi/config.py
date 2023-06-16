bba17e84f5ddb677edb25d9cb13a3c10cc80a487
MrRetyNine <we.are.beautiful@yandex.ru>
2023-06-16 11:06:21 +0300

    fix3

--- homework07/vkapi/config.py
+++ homework07/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "vk1.a.z8sECcGEBAbeaQ65LlWgquZgFZ1aMU5Y-Te1smoD74mFumYoL2JaXJCfTGm6TFqkwXr-h9wlQDXCA5OsUX1InOqqC8yKr40sekAc6k_xfl5z1aJ_KPhtoL-K3ZVc9UEM4maoL9tDj8JZRfv8R_5zaddBZyvkmbDaWYOUnsGfVjFvea8Xj9NtAM3xUgJs9Jc7",
     "version": "5.126",
 }
