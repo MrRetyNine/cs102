d536b9e05b72167acd3881a61bc192737ddd85bb
MrRetyNine <we.are.beautiful@yandex.ru>
2023-06-16 11:00:30 +0300

    fix2

--- homework07/vkapi/config.py
+++ homework07/vkapi/config.py
@@ -1,17 +1,7 @@
 # https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
-import os.path
-
-from dotenv import load_dotenv  # type: ignore
-
-path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")), ".env")
-if os.path.exists(path):
-    load_dotenv(path)
-    token = os.environ.get("token")
-else:
-    raise FileNotFoundError
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": token,
+    "access_token": "",
     "version": "5.126",
 }
